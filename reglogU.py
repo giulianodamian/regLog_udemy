c75ab2e2adffdc66ac8c5b3d678a0566890a9743
Giuliano <fsc.damian@gmail.com>
2024-11-08 19:04:01 -0300

    Extração dos dados: ver caminho da pasta no computador

--- reglogU.py
+++ reglogU.py
@@ -1,1 +1,13 @@
-#teste
+#!usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+
+##Extração dos dados##
+doencas_pre = pd.read_csv('~/Documentos/dados/dadosRegLog.csv',sep=';', encoding='utf-8')
+
+##Análise Inicial##
+
+print(doencas_pre.head())
+doencas_pre.shape
